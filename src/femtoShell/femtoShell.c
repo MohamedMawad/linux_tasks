4b5b3f4845d63f64078ef5ef21d2ffd3b02cd365
MohamedMawad <mohamedmoawad1995@gmail.com>
2022-10-07 16:15:05 +0200

    Adding cd built-in command

--- src/femtoShell/femtoShell.c
+++ src/femtoShell/femtoShell.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <limits.h>
+#include <signal.h>
 
 // printf color codes
 void red() { printf("\033[1;31m"); }
@@ -44,6 +45,13 @@ int tokenizer(char *inputStr, char separator, char ***output)
 {
 	int len = strlen(inputStr);
 	char *cpdInputStr = (char *)malloc(len + 1);
+
+	if (cpdInputStr == NULL)
+	{
+		perror("malloc() call in str_separate");
+		exit(1);
+	}
+
 	strcpy(cpdInputStr, inputStr);
 	int numTokens = 0;
 	char **currentToken;
@@ -95,7 +103,7 @@ int tokenizer(char *inputStr, char separator, char ***output)
 		}
 	}
 
-	// Adding the pointer to NULL teminator at the last element of the output
+	// Adding the pointer to NULL at the last element of the output
 	currentToken++;
 	*currentToken = NULL;
 	return numTokens;
@@ -111,22 +119,79 @@ int main(int argc, char **argv)
 	ssize_t lineSize = 0;
 	pid_t ret_wait, ch_pid;
 	int wstatus;
+	// Ignoring the following signals Ctrl+C(SIGINT), Ctrl+Z(SIGTSTP), Ctrl+\(SIGQUIT)
+	signal(SIGINT, SIG_IGN);
+	signal(SIGTSTP, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
 
 	while (1)
 	{
 		printPrompt();
 		// gets(input);
 		lineSize = getline(&line, &len, stdin);
+		// Handling the empty line from the user
+		if (lineSize == 1ll)
+		{
+			continue;
+		}
 		line[lineSize - 1] = '\0';
 		cmdArgc = tokenizer(line, ' ', &cmdArgv);
 		// for (int i = 0; i < cmdArgc; i++)
 		// {
 		// 	printf("argv[%d]: %s\n", i, cmdArgv[i]);
 		// }
-		if (strcmp(line, "exit") == 0)
+		if (strcmp(cmdArgv[0], "exit") == 0)
 		{
 			printf("Good Bye :) \n");
 			break;
+		}
+		else if (strcmp(cmdArgv[0], "cd") == 0)
+		{
+			static char priviousPath[PATH_MAX + 1] = {0};
+			char currentPath[PATH_MAX + 1];
+			int status = 0;
+
+			getcwd(currentPath, PATH_MAX + 1);
+
+			if (cmdArgc == 1)
+			{
+				chdir(getenv("HOME"));
+			}
+			else if (cmdArgc > 2)
+			{
+				printf("cd: too many arguments\n");
+				status = -1;
+			}
+			else if (strcmp(cmdArgv[1], "-") == 0)
+			{
+
+				if ((priviousPath[0] != 0))
+				{
+					if (chdir(priviousPath) == -1)
+					{
+						printf("cd: %s: No such file or directory\n", priviousPath);
+						status = -1;
+					}
+				}
+				else
+				{
+					printf("cd: OLDPWD not set\n");
+					status = -1;
+				}
+			}
+			else if (chdir(cmdArgv[1]) == -1)
+			{
+				printf("cd: %s: No such file or directory\n", cmdArgv[1]);
+				status = -1;
+			}
+
+			if (status == 0)
+			{
+				strcpy(priviousPath, currentPath);
+				priviousPath[strlen(currentPath)] = 0;
+			}
+			
+			continue;
 		}
 
 		ch_pid = fork();
@@ -134,9 +199,9 @@ int main(int argc, char **argv)
 		if (ch_pid == 0)
 		{
 			// child code
-			printf("child:\n");
-			printf("pid = %d, ppid = %d\n", getpid(), getppid());
-			printf("ch_pid = %d\n", ch_pid);
+			// printf("child:\n");
+			// printf("pid = %d, ppid = %d\n", getpid(), getppid());
+			// printf("ch_pid = %d\n", ch_pid);
 			execvp(cmdArgv[0], cmdArgv);
 			printf("%s Not a command\n", line);
 			exit(0);
@@ -144,9 +209,9 @@ int main(int argc, char **argv)
 		else if (ch_pid > 0)
 		{
 			// patrent code
-			printf("parent:\n");
-			printf("pid = %d, ppid = %d\n", getpid(), getppid());
-			printf("ch_pid = %d\n", ch_pid);
+			// printf("parent:\n");
+			// printf("pid = %d, ppid = %d\n", getpid(), getppid());
+			// printf("ch_pid = %d\n", ch_pid);
 			// sleep(20);
 			ret_wait = wait(&wstatus);
 		}
@@ -156,8 +221,9 @@ int main(int argc, char **argv)
 			exit(EXIT_FAILURE);
 		}
 
-		printf("pid = %d, ppid = %d\n", getpid(), getppid());
-		printf("You Said: %s\n", line);
+		// printf("pid = %d, ppid = %d\n", getpid(), getppid());
+		// printf("%ld\n", lineSize);
+		// printf("You Said: %s\n", line);
 
 		free(cmdArgv[0]);
 		free(cmdArgv);
@@ -167,151 +233,3 @@ int main(int argc, char **argv)
 
 	return 0;
 }
-
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include <sys/types.h>
-// #include <unistd.h>
-// #include <sys/wait.h>
-// #include <limits.h>
-
-// // int main () {
-// //   red();
-// //   printf("Hello ");
-// //   yellow();
-// //   printf("world\n");
-// // 	blue();
-// //   printf("Hello ");
-// //   green();
-// //   printf("world\n");
-// //   reset();
-// //   return 0;
-// // }
-
-// // printf color codes
-// #define RED \033[1;31m
-// // void green() { printf("\033[1;32m"); }
-// // void yellow() { printf("\033[1;33m"); }
-// // void blue() { printf("\033[1;34m"); }
-// #define RESET \033[0m
-
-// int main(int argc, char **argv)
-// {
-
-// 	char username[NAME_MAX + 1];
-// 	char hostname[_SC_HOST_NAME_MAX + 1];
-// 	char fullPath[PATH_MAX + 1];
-// 	// size_t length;
-
-// 	getlogin_r(username, NAME_MAX + 1);
-// 	gethostname(hostname, _SC_HOST_NAME_MAX + 1);
-// 	getcwd(fullPath, PATH_MAX + 1);
-
-// 	// green();
-// 	printf("RED%s@RESET%s", username, hostname);
-// 	// reset();
-// 	// printf(":");
-// 	// blue();
-// 	// printf("%s", fullPath);
-// 	// reset();
-// 	// printf("# ");
-// 	return 0;
-// }
-
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-
-// #define BUFSIZE 100
-
-// int str_separate(char *cpdInputStr, char separator, char ***output)
-// {
-// 	int len = strlen(cpdInputStr);
-// 	int numTokens = 0;
-// 	char **currentToken;
-// 	int i;
-// 	for (i = 0; i < len; i++)
-// 	{
-// 		if ((cpdInputStr[i] == separator))
-// 		{
-// 			cpdInputStr[i] = '\0';
-// 			numTokens++;
-// 			for (int j = i + 1; j < len - 1; j++)
-// 			{
-// 				if (cpdInputStr[j] != separator)
-// 					break;
-// 				else
-// 					i = j;
-// 			}
-// 		}
-// 	}
-// 	numTokens++;
-// 	*output = malloc(numTokens * sizeof(char *));
-// 	if (*output == NULL)
-// 	{
-// 		perror("malloc() call in str_separate");
-// 		exit(1);
-// 	}
-// 	currentToken = *output;
-// 	*currentToken = cpdInputStr;
-// 	for (i = 0; i < len; i++) /* < len: we never get to the last '\0',
-// 							   * since strlen gives us the length of
-// 							   * the string, not including the NULL */
-// 	{
-// 		if (cpdInputStr[i] == '\0')
-// 		{
-// 			currentToken++;
-// 			*currentToken = &(cpdInputStr[i + 1]);
-// 		}
-// 	}
-// 	return numTokens;
-// }
-
-// int main(int argc, char **argv)
-// {
-// 	char buf[BUFSIZE];
-// 	char **mystrptrs;
-// 	char separator = ' ';
-// 	int numTokens;
-// 	int j;
-// 	strncpy(buf, "CSC209 Winter  2008 taught by Karen Reid", 100);
-
-// 	/* Just to prove to ourselves that the string is really in there... */
-// 	printf("Contents of buf: %s\n", buf);
-// 	printf("Address of buf: %p\n", buf);
-
-// 	numTokens = str_separate(buf, ' ', &mystrptrs);
-
-// 	/* Print out the number of tokens we found */
-// 	printf("Tokens found with delimiter '%c': %d\n\n", separator, numTokens);
-
-// 	/* Header for our printout */
-// 	printf("Token                        ");
-// 	printf("Word                     Starting address\n");
-// 	printf("-----------------------------");
-// 	printf("-----------------------------------------\n");
-// 	for (j = 0; j < numTokens; j++)
-// 	{
-// 		printf("%5d\t%30s\t%30p\n", j + 1, mystrptrs[j], mystrptrs[j]);
-// 	}
-
-// 	/* We'll print this again just to show that buf was modified */
-// 	printf("\nContents of buf: %s\n", buf);
-// 	printf("(will only display until the first NULL character)\n");
-
-// 	/*
-// 	 * str_separate made a call to malloc(); you should make it a
-// 	 * practice to always make sure you free() what you malloc()
-// 	 * when you're done with it, even though it strictly doesn't
-// 	 * matter in this case since we're just exiting.
-// 	 *
-// 	 * DON'T:
-// 	 *   - try to free() memory you didn't allocate.
-// 	 *   - try to free() things that have already been free()'d.
-// 	 *   - try to dereference a pointer to memory you've free()'d
-// 	 */
-
-// 	free(mystrptrs);
-// 	return 0;
-// }
